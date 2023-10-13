3efa13712a8a6689333ef802336900572c7a534f
shubhamjha935 <shubhamjha935@gmail.com>
2023-10-13 11:09:39 +0530

    add new file

---
+++ stackAndHeap.js
@@ -0,0 +1,13 @@
+let name = "shubham jha"
+let anothername = name;
+ anothername = "hitesh"
+console.log(name);
+console.log(anothername);
+let userone = {
+    email : "user@google.com",
+    upi  : "user@ybl"
+}
+let usertwo = userone;
+usertwo.email = "shubhamjha2574@gmail.com"
+console.log(userone.email);
+console.log(usertwo.email);
