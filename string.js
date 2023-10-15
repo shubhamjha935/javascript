3d6cfa99b082b5e175a2adde2a196778bfeb375b
shubhamjha935 <shubhamjha935@gmail.com>
2023-10-15 12:16:12 +0530

    change in array

---
+++ string.js
@@ -0,0 +1,8 @@
+const name = "shubham";
+const represent = 50;
+console.log(name+represent);
+console.log(`hello my name is ${name} and my repo count is ${represent}`);
+const gamename = new String("shubham");
+console.log(gamename[0]);
+console.log(gamename.__proto_);
+
