5249e235182216803a6002998afc386b573d9f30
shubhamjha935 <shubhamjha935@gmail.com>
2023-10-18 09:28:19 +0530

    completed all array

--- array.js
+++ array.js
@@ -12,15 +12,34 @@ myArr.unshift(9);
 myArr.shift()
 // console.log(myArr.includes(9))
 // console.log(myArr.indexOf(3))
-const newArr = myArr.join()
-console.log(newArr)
-console.log(typeof newArr)
-//slice splice 
-console.log("A",myArr);
-const myn1 = (myArr.slice(1,3))
-console.log(myn1)
-console.log("b",myArr);
-const myn2 = (myArr.splice(1,3))
-console.log(myn2);
-console.log(myArr)
+// const newArr = myArr.join()
+// console.log(newArr)
+// console.log(typeof newArr)
+// //slice splice 
+// console.log("A",myArr);
+// const myn1 = (myArr.slice(1,3))
+// console.log(myn1)
+// console.log("b",myArr);
+// const myn2 = (myArr.splice(1,3))
+// console.log(myn2);
+// console.log(myArr)
 
+const marvel_heros = ["thor","ironman","spiderman"]
+const dc_heros =["superman","flash","batman"]
+//marvel_heros.push(dc_heros)
+//console.log(marvel_heros[3][1])
+//const all_heros = marvel_heros.concat(dc_heros)
+//console.log(marvel_heros)
+//console.log(all_heros)
+//const all_new_heros = [...marvel_heros,...dc_heros]
+//console.log(all_new_heros);
+//const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]] 
+//const real_another_array = another_array.flat(Infinity);
+//console.log(real_another_array);
+console.log(Array.isArray("Hitesh"))
+console.log(Array.from("Hitesh"))
+console.log(Array.from({name:"hitesh"}))
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1,score2,score3));
--- first.java
+++ first.java
@@ -8,4 +8,5 @@ public class first {
     public static void main(String[] args) {
         System.out.println("hi shubham");
     }
+    
 }
